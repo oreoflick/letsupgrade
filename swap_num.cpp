e6d6ba98b13ae5fbf2cdbc6f1be7fc2ad5724f6c
Ankur Patra <102987184+oreoflick@users.noreply.github.com>
2023-03-28 20:59:46 +0530

    Create swap_num.cpp

---
+++ swap_num.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2;
+
+  // Getting input from user
+  cout << "Enter the first number: ";
+  cin >> num1;
+  cout << "Enter the second number: ";
+  cin >> num2;
+
+  // Swapping the numbers
+  num1 = num1 + num2;
+  num2 = num1 - num2;
+  num1 = num1 - num2;
+
+  // Printing the swapped numbers
+  cout << "After swapping, the first number is: " << num1 << endl;
+  cout << "After swapping, the second number is: " << num2 << endl;
+
+  return 0;
+}
